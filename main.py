064fe14cad83772984568439a8531bbb43b3c37d
Christian Camp <crdcamp@gmail.com>
2026-05-05 11:59:13 -0600

    Before trying new imports

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.llms import LlamaCpp
