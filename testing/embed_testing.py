dbe45d796bf822660ce7824e1d3d47029a0bd16b
Christian Camp <crdcamp@gmail.com>
2026-05-05 13:14:21 -0600

    Test model embedding working

---
+++ testing/embed_testing.py
@@ -0,0 +1,36 @@
+# %% Imports
+from llama_cpp import Llama
+
+# %% Model
+llm = Llama(
+    model_path="models/Qwen3-Embedding-8B-Q6_K.gguf",
+    embedding=True,
+    n_ctx=20480,)
+
+# %% Open one file for single document embedding
+first_doc = "data/summary/httpsawsamazoncomwhatisvectordatabases.md"
+with open(first_doc, 'r', encoding='utf-8') as f:
+    first_doc_content = f.read()
+
+print(type(first_doc_content))
+
+# %% Create single document embedding
+embeddings = llm.create_embedding(first_doc_content)
+
+# %% Inspect
+print(type(embeddings))
+print(embeddings)
+
+
+# %% Read second document
+second_doc = "data/summary/httpsblogapifycomwhatisavectordatabase.md"
+with open(second_doc, 'r', encoding='utf-8') as f2:
+    second_doc_content = f2.read()
+
+print(type(second_doc_content))
+
+# %% Multiple document embedding
+embeddings = llm.create_embedding([first_doc_content, second_doc_content])
+
+# %% Inspect
+print(type(embeddings))
