c922d36f808e5490b1eace759e37d250bbefed3f
Christian Camp <crdcamp@gmail.com>
2026-05-05 14:33:37 -0600

    Saving json for inspection

--- testing/embedding_similarity_testing.py
+++ testing/embedding_similarity_testing.py
@@ -2,6 +2,8 @@
 # %% Imports
 from llama_cpp import Llama
 from sklearn.metrics.pairwise import cosine_similarity
+import os
+import json
 
 # %% Model
 llm = Llama(
@@ -17,7 +19,16 @@ with open(first_doc, 'r', encoding='utf-8') as f1, open(second_doc, 'r', encodin
     second_doc_str = f2.read()
 
 # %% Embed both files
-embeddings = llm.create_embedding([first_doc_str, second_doc_str])
+first_doc_embeddings = llm.create_embedding(first_doc_str)
+second_doc_embeddings = llm.create_embedding(second_doc_str)
+
+# %% Inspect embeddings type
+print(type(first_doc_embeddings), "\n", type(second_doc_embeddings))
+# %% Save dictionaries to inspect dict structure
+os.makedirs("testing", exist_ok=True)
+with open('testing/first_doc_embeddings_dict.json', 'w') as dict1, open('testing/second_doc_embeddings_dict.json', 'w') as dict2:
+    json.dump(first_doc_embeddings, dict1)
+    json.dump(second_doc_embeddings, dict2)
 
 # %% Cosine Similarity
 # Compare the cosine similarity between the two embedded documents
