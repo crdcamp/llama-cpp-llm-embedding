910ef9807566463559d449843d6c53a67864fe4a
Christian Camp <crdcamp@gmail.com>
2026-05-05 11:28:24 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# %% Imports
+import os
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.llms import LlamaCpp
+import requests
+from tqdm import tqdm
+import time
+
+model_path = "models/Qwen2.5-7B-Instruct-Q4_K_M.gguf"
