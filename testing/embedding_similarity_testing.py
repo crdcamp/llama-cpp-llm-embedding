3953b62eaad6cd3b091c3ec5fe70b651d955f2ac
Christian Camp <crdcamp@gmail.com>
2026-05-05 19:40:15 -0600

    Baseline embedding function complete. Chunking needs to be added

--- testing/embedding_similarity_testing.py
+++ testing/embedding_similarity_testing.py
@@ -97,8 +97,8 @@ for file in os.listdir(docs_dir):
 
     end_time = time.perf_counter()
     total_time = end_time - start_time
-
-    print(f"Processed {file} in {total_time:.2f} seconds\n\n")
     times.append(total_time)
+    print(f"Processed {file} in {total_time:.2f} seconds\n\n")
 
-print(f"Total embedding time: {sum(times)}")
+print("ALL DONE!")
+print(f"Total embedding time: {sum(times):.2f} seconds")
