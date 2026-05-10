e2f83d6b5146c0e7d24567ca7442ccb4dabcee4b
Christian Camp <crdcamp@gmail.com>
2026-05-09 19:02:03 -0600

    Converting embedding to function

--- testing/single_embedding_testing.py
+++ testing/single_embedding_testing.py
@@ -5,6 +5,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 import time
 import numpy as np
 
+# %% Test file
+test_file = "../data/summary/httpswwwdatabrickscomblogwhatisvectordatabase.md"
+
 # %% Model
 context_length = 40960
 llm = Llama(
@@ -16,21 +19,40 @@ llm = Llama(
 
 # %% Text splitter
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=200,
+    chunk_size=100,
     chunk_overlap=50,
     length_function=len,
     is_separator_regex=False,
 )
 
 # %% Test text split
-test_file = "../data/summary/httpswwwdatabrickscomblogwhatisvectordatabase.md"
 with open(test_file, 'r', encoding='utf-8') as f:
     # Initial reading and data inspection
     test_content = f.read()
     test_documents = text_splitter.create_documents([test_content])
     print(f"Test documents type: {type(test_documents)}")
+
     contents = [doc.page_content for doc in test_documents]
+    print(f"Contents type: {type(contents)}")
 
     for item in contents:
         embeddings = llm.create_embedding(item)
         print(f"Embeddings type: {type(embeddings)}")
+
+# %% Conversion to function
+def embed_file(file):
+    start_time = time.perf_counter()
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        split_text = text_splitter.create_documents([text])
+        documents = [doc.page_content for doc in split_text]
+        print(type(documents))
+
+        for doc in documents:
+            embeddings = llm.create_embedding(doc)
+
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f"Documents embedded in {elapsed_time:.2f} seconds")
+
+test_embed = embed_file(test_file)
