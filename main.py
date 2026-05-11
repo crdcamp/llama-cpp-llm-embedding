8957fb4032a770a79a81e379b0e17516e79bcb4d
Christian Camp <crdcamp@gmail.com>
2026-05-11 14:46:02 -0600

    Before removing token split

--- main.py
+++ main.py
@@ -6,20 +6,42 @@ import time
 import numpy as np
 
 # %% Model
-context_window = 20480
+"""
+Gonna abandon using text splitting on the documents for now.
+
+I have yet to confirm but I think text splitting might mess with the
+embedding results.
+
+So, we're gonna just leave this as a stripped down idea where
+the embedding function checks the length for our (short) context
+window.
+"""
+context_window = 4096
 llm = Llama(
     model_path="models/Qwen3-Embedding-8B-Q6_K.gguf",
     embedding=True,
     verbose=True,
     n_ctx=context_window,
-    n_batch=context_window # IN ACTUAL USE CASE: Leave this at 512 and encode the text using batches instead
+    n_batch=context_window # Need to double check if this is a good idea...
 )
 
 # %% Text splitter
+# Not sure if I need it here given the token text length check...
+# Hmmmm ......
+text_splitter_chunk_size = 2048
 text_splitter = TokenTextSplitter(
-    chunk_size=2048,
+    chunk_size=text_splitter_chunk_size,
     chunk_overlap=100,
 )
+
+"""
+Instead of a text splitter, I might just do a token
+check here. I'm not sure how necessary the text split is in comparison to
+the context of the text.
+
+I'm still not even sure if the batches mess with context, or even in what way...
+Needs some research
+""";
 
 # %% Embed Function
 def embed_file(file: str, context_window: int):
@@ -28,34 +50,25 @@ def embed_file(file: str, context_window: int):
         text = f.read()
 
         # Token length calculation
-        token_text = text.encode('utf-8')
-        text_token_len = len(llm.tokenize(token_text, add_bos=False))
-        print(f"File: {file} token length: {text_token_len}")
+        text_token = text.encode('utf-8') # Some reason `.tokenize()` gets upset without this. Involves a utf encoding error I think
+        text_token_len = len(llm.tokenize(text_token, add_bos=False))
+        if text_token_len > text_splitter_chunk_size:
+            print(f"Error: File {file} exceeded chunk size: {text_token_len}. Abandoning...")
+            return
+        else:
+            del text_token, text_token_len # Won't be needing these from here on out
+            # Split text
+            split_text = text_splitter.create_documents([text])
+            documents = [doc.page_content for doc in split_text]
+            documents_embeddings = []
 
-        # Split text
-        split_text = text_splitter.create_documents([text])
-        documents = [doc.page_content for doc in split_text]
-        documents_embeddings = []
-
-        for doc in documents:
-            embeddings = llm.create_embedding(doc) # For some reason list comprehension won't work here
-
-            """
-            Not entirely necessary to include this part below... but we'll include it just in case.
-            The logic for referencing context window also might be incorrect.
-            You'll also want to calculate the tokens earlier in the code as well
-            and use this logic earlier, but we'll leave this as is for now
-            """
-            embedding_token_usage = embeddings['usage']['total_tokens']
-            if embedding_token_usage <= context_window:
+            for doc in documents:
+                embeddings = llm.create_embedding(doc) # For some reason list comprehension won't work here
                 documents_embeddings.extend(embeddings["data"])
-                print(f"TOTAL TOKENS: {embeddings['usage']['total_tokens']}")
-            else:
-                print(f"File {file} exceeded context window. Skipping...")
-                continue
+
 
     end_time = time.perf_counter()
-    print(f"Documents embedded in {end_time - start_time:.2f} seconds")
+    print(f"File: {file} embedded in {end_time - start_time:.2f} seconds")
 
     return documents_embeddings
 
@@ -64,13 +77,6 @@ test_doc = "data/summary/httpsawsamazoncomwhatisvectordatabases.md"
 
 # %% Embed
 test_documents_embeddings = embed_file(test_doc, context_window=context_window)
-
-# %% Inspecting
-print("test_documents_embeddings PROPERTIES")
-print("Type: ", type(test_documents_embeddings))
-print("Length: ", len(test_documents_embeddings))
-#print("Test vector sample:\n", test_documents_embeddings[0:1], "\n")
-
 
 """
 Need to do aggregate testing before continuing here
