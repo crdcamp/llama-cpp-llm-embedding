6de1c99d5c95ab5abaa371ada538fc53fb7f0607
Christian Camp <crdcamp@gmail.com>
2026-05-12 15:59:42 -0600

    Before doing something stupid

--- testing/chroma_db_testing.py
+++ testing/chroma_db_testing.py
@@ -23,38 +23,6 @@ llm = Llama(
     n_batch=context_window # Need to double check if this is a good idea... Probably not a good idea...
 )
 
-# %% Embed Function
-def embed_file(file: str, context_window: int):
-    with open(file, 'r', encoding='utf-8') as f:
-        text = f.read()
-        # Document token length check
-        text_token = text.encode('utf-8') # Some reason `.tokenize()` gets upset without this. Involves a utf encoding error I think
-        text_token_len = len(llm.tokenize(text_token, add_bos=False))
-        if text_token_len > context_window:
-            print(f"Error: File `{file}` exceeded context window: {text_token_len}. Abandoning...\n\n")
-            return None
-        else: # Embed the file
-            start_time = time.perf_counter()
-            print(f"Processing File: `{file}` with token length: {text_token_len}")
-            embeddings = llm.create_embedding(text)
-            end_time = time.perf_counter()
-            elapsed_time = end_time - start_time
-            print(f"File `{file}` embedded in {elapsed_time:.2f} seconds at {text_token_len / (elapsed_time):.2f} tokens/second\n\n")
-
-            return embeddings
-
-# %% Test embedding
-test_doc = "../data/summary/httpsawsamazoncomwhatisvectordatabases.md"
-test_embedding = embed_file(test_doc, context_window=context_window)
-
-# %% Inspect and access only embeddings
-print(type(test_embedding))
-for key, value in test_embedding.items():
-    print(key)
-
-# %% Custom embedding function
-# Start here: https://docs.trychroma.com/docs/embeddings/embedding-functions#custom-embedding-functions
-# From: https://github.com/chroma-core/chroma/issues/2409
 @register_embedding_function
 class LlamaCppEmbeddingFunction(EmbeddingFunction):
 
@@ -73,16 +41,11 @@ class LlamaCppEmbeddingFunction(EmbeddingFunction):
     def get_config(self) -> Dict[str, Any]:
         return dict(model_path=self.model_path)
 
+    # This needs to be double checked
     @staticmethod
     def build_from_config(config: Dict[str, Any]) -> "LlamaCppEmbeddingFunction":
         model = Llama(model_path=config['model_path'], embedding=True)
         return LlamaCppEmbeddingFunction(model=model, model_path=config['model_path'])
-
-# %%
-all_test_embeddings = [item['embedding'] for item in test_embedding['data']]
-# Flatten list (was a nested list before)
-all_test_embeddings = list(itertools.chain.from_iterable(all_test_embeddings))
-print(len(all_test_embeddings))
 
 # %% Initialize ChromaDB
 # We'll use PesistentClient outside of testing
