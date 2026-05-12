0a7d48d92baf0c0c2da604326e00ed77df51b454
Christian Camp <crdcamp@gmail.com>
2026-05-12 12:25:25 -0600

    Test custom embedding function for ChromaDB

--- testing/chroma_db_testing.py
+++ testing/chroma_db_testing.py
@@ -1,17 +1,14 @@
 # %% Imports
+from multiprocessing.sharedctypes import Value
 import os
 from posixpath import exists
 from llama_cpp import Llama
 import time
 import numpy as np
 import chromadb
-
-# %% Chromadb
-test_db_path = "test_db"
-os.makedirs(test_db_path, exist_ok=True)
-
-client = chromadb.PersistentClient(path=test_db_path)
-collection = client.get_or_create_collection(name="vector-database")
+from typing import Dict, Any
+from chromadb import Documents, EmbeddingFunction, Embeddings
+from chromadb.utils.embedding_functions import register_embedding_function
 
 # %% Model
 context_window = 2048
@@ -27,7 +24,7 @@ llm = Llama(
 def embed_file(file: str, context_window: int):
     with open(file, 'r', encoding='utf-8') as f:
         text = f.read()
-        # Token length check
+        # Document token length check
         text_token = text.encode('utf-8') # Some reason `.tokenize()` gets upset without this. Involves a utf encoding error I think
         text_token_len = len(llm.tokenize(text_token, add_bos=False))
         if text_token_len > context_window:
@@ -43,13 +40,44 @@ def embed_file(file: str, context_window: int):
 
             return embeddings
 
+# %% Chromadb
+@register_embedding_function
+class MyEmbeddingFunction(EmbeddingFunction):
+
+    def __init__(self, model, context_window: int):
+        self.model = model
+        self.context_window = context_window
+
+    def __call__(self, input: Documents) -> Embeddings:
+        results = []
+        for doc in input:
+            embedding = embed_file(doc, self.context_window)
+            if embedding is None:
+                raise ValueError(f"Failed to embed document: {doc}")
+            results.append(embedding["data"][0]["embedding"])
+        return results
+
+    @staticmethod
+    def name() -> str:
+        return "my-ef"
+
+    def get_config(self) -> Dict[str, Any]:
+        return {"model": self.model, "context_window": self.context_window}
+
+    @staticmethod
+    def build_from_config(config: Dict[str, Any]) -> "EmbeddingFunction":
+        return MyEmbeddingFunction(config["model"], config["context_window"])
+
+test_db_path = "test_db"
+os.makedirs(test_db_path, exist_ok=True)
+
+my_ef = MyEmbeddingFunction(model=llm, context_window=context_window)
+client = chromadb.PersistentClient(path=test_db_path)
+collection = client.get_or_create_collection(
+    name="test-database",
+    embedding_function=my_ef)
+
 # %% Test doc
 test_doc = "../data/summary/httpsawsamazoncomwhatisvectordatabases.md"
-
-# %% Test embedding
-test_embedding = embed_file(test_doc, context_window=context_window)
-
-# %% Inspect embeddings
-print(test_embedding)
 
 # %% Add to ChromaDB collection
