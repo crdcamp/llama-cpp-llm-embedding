ff9ed0c722f37065a14fcd6feafcf403d2a816d8
Christian Camp <crdcamp@gmail.com>
2026-05-14 17:47:27 -0600

    Testing out different rtext splitter calls

--- testing/text_splitter_testing.py
+++ testing/text_splitter_testing.py
@@ -4,19 +4,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 import chromadb
 from test_embed import LlamaCppEmbeddingFunction
 
-# %% Model Params
-embed_model_path = "../models/Qwen3-Embedding-8B-Q6_K.gguf"
-context_window = 2048
-verbose=True
-
-# %% Model
-embed_model = Llama(
-    model_path=embed_model_path,
-    embedding=True,
-    n_ctx=context_window,
-    verbose=verbose
-)
-
 # %% Single Test File
 file = "../data/summary/httpsblogapifycomwhatisavectordatabase.md"
 with open(file, 'r', encoding='utf-8') as f:
@@ -29,6 +16,24 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=50,
     length_function=len,
     is_separator_regex=False,
+)
+
+# %% Text splitter function testing
+split_texts = text_splitter.split_text(text)
+print(type(split_texts))
+print(split_texts)
+
+# %% Model Params
+embed_model_path = "../models/Qwen3-Embedding-8B-Q6_K.gguf"
+context_window = 2048
+verbose=True
+
+# %% Model
+embed_model = Llama(
+    model_path=embed_model_path,
+    embedding=True,
+    n_ctx=context_window,
+    verbose=verbose
 )
 
 # %% In-Memory Chroma DB
