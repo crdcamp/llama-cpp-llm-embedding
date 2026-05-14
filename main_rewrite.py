7810e1a78d25bb1493ef533bb1cb6defd49163ea
Christian Camp <crdcamp@gmail.com>
2026-05-14 12:06:12 -0600

    Ready to implement text splitter into collections creation

--- main_rewrite.py
+++ main_rewrite.py
@@ -2,15 +2,22 @@
 from llama_cpp import Llama
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import chromadb
-from typing import Dict, Any
-from chromadb import Documents, EmbeddingFunction, Embeddings
-from chromadb.utils.embedding_functions import register_embedding_function
 from datetime import datetime
 import os
+from embed import LlamaCppEmbeddingFunction
 
 # %% Model Params
+embed_model_path = "models/Qwen3-Embedding-8B-Q6_K.gguf"
 context_window = 2048
-model_path = "models/Qwen3-Embedding-8B-Q6_K.gguf"
+verbose=True
+
+# %% Model
+embed_model = Llama(
+    model_path=embed_model_path,
+    embedding=True,
+    n_ctx=context_window,
+    verbose=verbose
+)
 
 # %% Text Splitter
 text_splitter = RecursiveCharacterTextSplitter(
@@ -18,4 +25,26 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=50,
     length_function=len,
     is_separator_regex=False,
+)
+
+# %% Chroma DB Setup
+db_path = "chromadb"
+os.makedirs(db_path, exist_ok=True)
+client = chromadb.PersistentClient(path=db_path)
+
+collection = client.get_or_create_collection(
+    name="test-collection",
+    embedding_function=LlamaCppEmbeddingFunction(model=embed_model, model_path=model_path),
+    metadata={
+        "description": "A test collection for learning ChromaDB",
+        "created": str(datetime.now())
+    },
+    # More info on configuration: https://docs.trychroma.com/docs/collections/configure#what-is-an-hnsw-index
+    configuration={
+        "hnsw": {
+            "space": "cosine", # Turns out we don't need that cosine function from earlier
+            "ef_construction": 100, # 100 is the default value
+            "ef_search": 100, # 100 is the default value
+        }
+    }
 )
